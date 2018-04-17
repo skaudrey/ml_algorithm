e75e6dc6c32b0929f945f716769a8f3fb909b8c2
Skaudrey <Skaudrey@163.com>
2018-04-17 08:28:06 +0800

    lenet_construct

--- ANN/LeNet/LeNet-keras.py
+++ ANN/LeNet/LeNet-keras.py
@@ -7,33 +7,44 @@
 # @Software: PyCharm
 __author__ = 'MiaFeng'
 
-import keras.datasets.mnist
+from keras.datasets import mnist
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.layers.core import Dense,Activation,Flatten
 from keras.models import Sequential
-
+from keras.optimizers import Adam
+from keras.utils import np_utils    # change to one hot
 
 class CreateLeNet(object):
-    def createLeNet(self):
+    def createLeNet(self,input_shape,nb_class):
 
         conv_layers = [
-            Conv2D(filters=6,kernel_size=5,strides=2,padding='same',activation='relu'),
-            MaxPooling2D(pool_size=(2,2),strides=2),
-            Conv2D(filters=20, kernel_size=5, strides=2, padding='same', activation='relu'),
-            MaxPooling2D(pool_size=(2, 2), strides=2),
+            Conv2D(filters=6,kernel_size=5,strides=1,padding='same',activation='relu',input_shape=input_shape),
+            MaxPooling2D(pool_size=(2,2),strides=(2,2)),
+            Conv2D(filters=16, kernel_size=5, strides=1, padding='same', activation='relu'),
+            MaxPooling2D(pool_size=(2, 2), strides=(2,2)),
             Flatten()
         ]
 
         fc_layers = [
             Dense(120,activation='relu'),
-            Dense(10,activation='softmax')
+            Dense(nb_class,activation='softmax')
         ]
 
-        model = Sequential([conv_layers,fc_layers])
+        model = Sequential(conv_layers+fc_layers)
 
         return model
 
 # parameters
 VERBOSE = 1
 IMG_ROW,IMG_COL = 28,28
+NB_CLASS = 10
+BATCH_SIZE = 128
+OPTIMIZER = Adam()
+VALIDATION_SPLIT = 0.2
+INPUT_SHAPE = [IMG_ROW,IMG_COL,1]
+NB_EPOCH = 20
+
+
+# load mnist dataset
+(X_train, Y_train),(X_test, Y_test) = mnist.load_data()
