0572655a1e8ee248ffd0f2338210069c51950255
Skaudrey <Skaudrey@163.com>
2018-04-19 15:22:18 +0800

    nb

---
+++ naive-bayes/naivebayes.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/19 14:16
+# @Author  : MiaFeng
+# @Site    : 
+# @File    : naivebayes.py
+# @Software: PyCharm
+__author__ = 'MiaFeng'
