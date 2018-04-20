e4c65bf4592460a5510250ba17e3f541fdf3f0b5
Skaudrey <Skaudrey@163.com>
2018-04-20 11:17:49 +0800

    nb

--- naive-bayes/naivebayes.py
+++ naive-bayes/naivebayes.py
@@ -21,8 +21,8 @@ class NaiveBayes:
         '''
         vocabset = set([])
         for line in datalist:
-            print(set(line))
-            vocabset = vocabset | set(line)
+            # print(set(line))
+            vocabset = vocabset | set(line) #build set
         return list(vocabset)
 
     # the model of the vocabulary set of file
@@ -67,13 +67,16 @@ class NaiveBayes:
         numWords = len(trainMatrix[0])  # #features, which is the length of vocabulary dict theoretically
         pNeg = sum(trainLabel)/float(numTrainDocs)  # the probability of negative samples, which is denoted by 1 with the meaning of spam
 
+        # initialize to the numbers of classes for laplace smoother
         p0Num = np.ones(numWords)   # initialize the number of samples as 1 to avoiding the denomitor is zero
         p1Num = np.ones(numWords)   # the same target
 
-        p0InAll = 2.0   # the number of labels,which is used for laplace smoother
-        p1InAll = 2.0
+        # initialize to the numbers of classes for laplace smoother
+        nb_class = np.unique(trainLabel).shape[0]
+        p0InAll = nb_class * 1.0   # the number of labels,which is used for laplace smoother
+        p1InAll = nb_class * 1.0
 
-        # update the positive and negative samples in one specific file or the whole vocabulary dict
+        # counting the frequency of each words' appearance in all the training data, and the frequency of the words ia labeled by one specific class
         for i in range(numTrainDocs):
             if trainLabel[i] == 1:
                 p1Num += trainMatrix[i]
@@ -81,9 +84,10 @@ class NaiveBayes:
             else:
                 p0Num += trainMatrix[i]
                 p0InAll += sum(trainMatrix[i])
-        print("The number of spam: %d"%p1InAll)
+        # print("The number of spam: %d"%p1InAll)
 
         # log for avoiding underflow
+
         p0Vect = np.log(p0Num/p0InAll)
         p1Vect = np.log(p1Num/p1InAll)
         return p0Vect,p1Vect,pNeg
@@ -99,6 +103,50 @@ class NaiveBayes:
         '''
 
         prob_y0 = sum(vecSample * p0Vec) + np.log(1-pNeg)
-        prob_y1 = sum(vecSample * p0Vec) + np.log(pNeg)
+        prob_y1 = sum(vecSample * p1Vec) + np.log(pNeg)
 
+        # set the label with the label of maximum probability
+        if prob_y0>prob_y1:
+            return 0
+        else:
+            return 1
+
+    def testingNB(self,testsamples):
+        listPosts, listClasses = loadDataSet()
+        myVocabList = self._createVocabList(listPosts)
+        # print(myVocabList)
+
+        trainMat = []
+        for postInDoc in listPosts:
+            trainMat.append(self._bagOfWords2Vec(myVocabList,postInDoc))
+
+        p0v,p1v,pAb = self._trainNB(np.array(trainMat),np.array(listClasses))
+        # print(trainMat)
+        thisSample = np.array(self._bagOfWords2Vec(myVocabList,testsamples))
+        result = self._classifyNB(thisSample,p0v,p1v,pAb)
+        print("%s classified as : %d" % (testsamples, result))
+        return result
+
+
+
+def loadDataSet():
+    wordsList =[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                   ['my', 'dalmation', 'is', 'so', 'cute', ' and', 'I', 'love', 'him'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                   ['mr', 'licks','ate','my', 'steak', 'how', 'to', 'stop', 'him'],
+                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classLable = [0,1,0,1,0,1] # 0：good; 1:bad
+    return wordsList, classLable
+
+if __name__=='__main__':
+    clf = NaiveBayes()
+
+    testData =  [['dog', 'my', 'is', 'cute'],
+                 ['stupid', 'garbage'],
+                 ['Haha', 'I', 'really', 'Love', 'You'],
+                 ['This', 'is', 'my', 'dog']]
+
+    for item in testData:
+        clf.testingNB(item)
 
