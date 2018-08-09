53a590a3f74c06f82201bc9c8a844b49a6b2a50c
skaudrey <skaudrey@163.com>
2018-08-10 06:53:48 +0800

    simHash

---
+++ featureEngineering/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/10 6:50
+# @Author  : MiaFeng
+# @Contact : skaudrey@163.com
+# @Site    :
+# @File    : stacking.py
+# @Software: PyCharm
+__author__ = 'MiaFeng'
